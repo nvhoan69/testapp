60464f1accaec5d4def1bd1e8e9bfd6621f20fbb
Nguyễn Văn Hoàn <hoannv41@gmail.com>
2021-08-25 22:06:16 -0700

    change main.cpp, TestApp.pro.user, build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-/home/dev/Qt/5.14.2/gcc_64/bin/qmake TestApp.pro
+#/home/dev/Qt/5.14.2/gcc_64/bin/qmake TestApp.pro
+/home/thienkhutu/Qt5.14.2/5.14.2/gcc_64/bin/qmake TestApp.pro
 make clean
 make -j4
