37a1c499d218ca0fe9e405305e45a4a35e394e70
nvhoan69 <hoannv41@gmail.com>
2021-10-31 21:46:47 +0700

    add typedef, struct and union

--- main.cpp
+++ main.cpp
@@ -12,6 +12,19 @@
 #include "commonstore.h"
 #include "flux/dispatcher.h"
 
+#typedef vip1 = "V.I.P number 1"
+#typedef vip2 = "V.I.P number 2"
+
+struct product {
+	int weight;
+	int price;
+}
+
+union mydata {
+	int myInt;
+	float myFloat;
+}
+
 int foo(int a, int b) {
     int d = 1000;
     return d;
